e2239aa2063fe4b4fc07cc1764c957fe1f873d12
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2018-07-18 14:56:50 +0100

    check.experience working

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    ex = express(),
-    data = require('./data.json'),
-    i18n = require('./i18n.json'),
-    tools = require('./tools.js');
+  bodyParser = require('body-parser'),
+  ex = express(),
+  data = require('./data.json'),
+  i18n = require('./i18n.json'),
+  tools = require('./tools.js');
 
 const dialogflowFirebaseFulfillment = (request, response) => {
   const req = request,
@@ -17,24 +17,28 @@ const dialogflowFirebaseFulfillment = (request, response) => {
 
   let say = 'Sorry, I don\'t know how to answer that yet';
 
-  switch(action) {
-      case('what.experience'):
-          let skill = data.skill[params.skill],
-            year = dt.getFullYear(),
-            time = (year - parseInt(skill.date)) + 1,
-            phrase = i18n[action].say[Math.floor(Math.random() * i18n[action].say.length)],
-            replace = {
-                time: time,
-                plural: time > 1 ? 's' : '',
-                skill: params.skill
-            };
-          say = tools.stringReplace({input:phrase,repl:replace});
-          break;
+  if (action === 'what.experience' || action === 'check.experience') {
+    let skill = data.skill[params.skill] || false,
+      hasSkill = Boolean(skill).toString(),
+      year = dt.getFullYear(),
+      time = (year - parseInt(skill.date)) + 1,
+      phrase = i18n[action][hasSkill][Math.floor(Math.random() * i18n[action][hasSkill].length)],
+      replace = {
+        time: time,
+        plural: time > 1 ? 's' : '',
+        skill: params.skill
+      };
+    say = tools.stringReplace({
+      input: phrase,
+      repl: replace
+    });
   }
 
 
   //direct intents to proper functions
-  res.json(tools.botSay({input:say}));
+  res.json(tools.botSay({
+    input: say
+  }));
 };
 ex.use(bodyParser.json());
 ex.post('/', dialogflowFirebaseFulfillment);
